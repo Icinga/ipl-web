37af97c3fd91a89d8198b424b2bc72051260fb62
Johannes Meyer <johannes.meyer@icinga.com>
2020-07-03 15:51:00 +0200

    Suggestions: Limit entries and make them paginatable

--- src/Control/FilterEditor/Suggestions.php
+++ src/Control/FilterEditor/Suggestions.php
@@ -5,13 +5,36 @@ namespace ipl\Web\Control\FilterEditor;
 use ipl\Html\BaseHtmlElement;
 use ipl\Html\FormElement\InputElement;
 use ipl\Html\HtmlElement;
+use ipl\Stdlib\Contract\Paginatable;
+use ipl\Web\Url;
+use ipl\Web\Widget\Icon;
+use ipl\Web\Widget\Link;
+use IteratorIterator;
+use LimitIterator;
+use Traversable;
 
 class Suggestions extends BaseHtmlElement
 {
     protected $tag = 'ul';
 
-    /** @var iterable */
+    /** @var Url */
+    protected $url;
+
+    /** @var Traversable */
     protected $data;
+
+    /** @var int */
+    protected $limit = 25;
+
+    /** @var int */
+    protected $pageNo = 1;
+
+    public function setUrl(Url $url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
 
     public function setData($data)
     {
@@ -20,9 +43,26 @@ class Suggestions extends BaseHtmlElement
         return $this;
     }
 
+    public function setPageSize($pageNo, $limit)
+    {
+        $this->pageNo = $pageNo;
+        $this->limit = $limit;
+    }
+
     protected function assemble()
     {
-        foreach ($this->data as $term => $meta) {
+        $limit = $this->limit;
+        $offset = $this->limit * ($this->pageNo - 1);
+
+        if ($this->data instanceof Paginatable) {
+            $this->data->limit($limit);
+            $this->data->offset($offset);
+            $data = $this->data;
+        } else {
+            $data = new LimitIterator(new IteratorIterator($this->data), $offset, $limit);
+        }
+
+        foreach ($data as $term => $meta) {
             if (is_int($term)) {
                 $term = $meta;
             }
@@ -45,6 +85,26 @@ class Suggestions extends BaseHtmlElement
             }
 
             $this->add(new HtmlElement('li', null, new InputElement(null, $attributes)));
+        }
+
+        if (! $this->isEmpty() && $this->url !== null) {
+            $pagination = new HtmlElement('li', ['class' => 'pagination']);
+            if ($this->pageNo > 1) {
+                $pagination->add(new Link(new Icon('angle-double-left'), $this->url->with([
+                    'limit' => $limit,
+                    'page'  => $this->pageNo - 1
+                ])));
+            }
+
+            // TODO: Only add this if there's anything left
+            $pagination->add(new Link(new Icon('angle-double-right'), $this->url->with([
+                'limit' => $limit,
+                'page'  => $this->pageNo + 1
+            ])));
+
+            if (! $pagination->isEmpty()) {
+                $this->add($pagination);
+            }
         }
     }
 
