1332738c8e03c7ff62944af72e45d5f460015881
Johannes Meyer <johannes.meyer@icinga.com>
2020-07-29 10:50:13 +0200

    FilterEditor: Utilize `data-term-label` instead of `data-term`

--- src/Control/FilterEditor/Terms.php
+++ src/Control/FilterEditor/Terms.php
@@ -85,17 +85,17 @@ class Terms extends BaseHtmlElement
         }
     }
 
-    protected function assembleTerm($class, $type, $search, $term)
+    protected function assembleTerm($class, $type, $search, $label)
     {
         $this->add(new HtmlElement('label', [
             'data-term-index'   => $this->count(),
             'data-term-class'   => $class,
             'data-term-type'    => $type,
             'data-term-search'  => $search,
-            'data-term'         => $term
+            'data-term-label'   => $label
         ], new HtmlElement('input', [
             'type'  => 'text',
-            'value' => $term
+            'value' => $label
         ])));
     }
 }
