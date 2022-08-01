67f08dfe614f9abf6b61a5111e176d48b16ad9c2
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2022-08-01 11:44:59 +0200

    CompatForm: Use Translation trait

--- src/Compat/CompatForm.php
+++ src/Compat/CompatForm.php
@@ -3,10 +3,13 @@
 namespace ipl\Web\Compat;
 
 use ipl\Html\Form;
+use ipl\I18n\Translation;
 use ipl\Web\FormDecorator\IcingaFormDecorator;
 
 class CompatForm extends Form
 {
+    use Translation;
+
     protected $defaultAttributes = ['class' => 'icinga-form icinga-controls'];
 
     public function hasDefaultElementDecorator()
