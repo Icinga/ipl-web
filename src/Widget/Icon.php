b93beef99fb42cf80d3f5d8cc152d9c387f17263
Sukhwinder Dhillon <54990055+sukhwinder33445@users.noreply.github.com>
2023-04-21 12:36:07 +0200

    Make icon style adjustable (#137)
    ### Required in:
    https://github.com/Icinga/noma-web/pull/23

--- src/Widget/Icon.php
+++ src/Widget/Icon.php
@@ -12,21 +12,56 @@ class Icon extends BaseHtmlElement
 {
     protected $tag = 'i';
 
+    /** @var string Icon style */
+    protected $style;
+
+    /** @var string Icon default style */
+    protected $defaultStyle = 'fa';
+
     /**
-     * Create a icon element
+     * Create an icon element
      *
-     * Creates a icon element from the given name and HTML attributes. The icon element's tag will be <i>. The given
+     * Creates an icon element from the given name and HTML attributes. The icon element's tag will be <i>. The given
      * name will be used as automatically added CSS class for the icon element in the format 'icon-$name'. In addition,
      * the CSS class 'icon' will be automatically added too.
      *
      * @param string           $name       The name of the icon
      * @param Attributes|array $attributes The HTML attributes for the element
      */
-    public function __construct($name, $attributes = null)
+    public function __construct(string $name, $attributes = null)
     {
         $this
             ->getAttributes()
-                ->add('class', ['icon', 'fa', "fa-$name"])
+                ->add('class', ['icon', "fa-$name"])
                 ->add($attributes);
+    }
+
+    /**
+     * Get the icon style
+     *
+     * @return string
+     */
+    public function getStyle(): string
+    {
+        return $this->style ?? $this->defaultStyle;
+    }
+
+    /**
+     * Set the icon style
+     *
+     * @param string $style Style class with prefix
+     *
+     * @return $this
+     */
+    public function setStyle(string $style): self
+    {
+        $this->style = $style;
+
+        return $this;
+    }
+
+    protected function assemble()
+    {
+        $this->addAttributes(['class' => $this->getStyle()]);
     }
 }
