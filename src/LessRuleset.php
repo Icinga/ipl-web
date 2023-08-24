54e91c362fd9b63ecc5b362ca81e26534620099a
Johannes Meyer <johannes.meyer@icinga.com>
2023-08-24 15:22:54 +0200

    LessRuleset: Add documentation

--- src/LessRuleset.php
+++ src/LessRuleset.php
@@ -5,14 +5,25 @@ namespace ipl\Web;
 use ArrayObject;
 use Less_Parser;
 
+/**
+ * @extends ArrayObject<string, string>
+ */
 class LessRuleset extends ArrayObject
 {
     /** @var ?string */
     protected $selector;
 
-    /** @var list<LessRuleset> */
+    /** @var array<LessRuleset> */
     protected $children = [];
 
+    /**
+     * Create a new LessRuleset
+     *
+     * @param string $selector Selector to use
+     * @param array<string, string> $properties CSS properties
+     *
+     * @return self
+     */
     public static function create(string $selector, array $properties): self
     {
         $ruleset = new static();
@@ -22,11 +33,23 @@ class LessRuleset extends ArrayObject
         return $ruleset;
     }
 
+    /**
+     * Get the selector
+     *
+     * @return ?string
+     */
     public function getSelector(): ?string
     {
         return $this->selector;
     }
 
+    /**
+     * Set the selector
+     *
+     * @param string $selector
+     *
+     * @return $this
+     */
     public function setSelector(string $selector): self
     {
         $this->selector = $selector;
@@ -34,11 +57,26 @@ class LessRuleset extends ArrayObject
         return $this;
     }
 
+    /**
+     * Get a property value
+     *
+     * @param string $property Name of the property
+     *
+     * @return string
+     */
     public function getProperty(string $property): string
     {
-        return $this[$property];
+        return (string) $this[$property];
     }
 
+    /**
+     * Set a property
+     *
+     * @param string $property Name to use
+     * @param string $value Value to set
+     *
+     * @return $this
+     */
     public function setProperty(string $property, string $value): self
     {
         $this[$property] = $value;
@@ -46,11 +84,23 @@ class LessRuleset extends ArrayObject
         return $this;
     }
 
+    /**
+     * Get all properties
+     *
+     * @return array<string, string>
+     */
     public function getProperties(): array
     {
         return $this->getArrayCopy();
     }
 
+    /**
+     * Set properties
+     *
+     * @param array<string, string> $properties
+     *
+     * @return $this
+     */
     public function setProperties(array $properties): self
     {
         $this->exchangeArray($properties);
@@ -58,6 +108,14 @@ class LessRuleset extends ArrayObject
         return $this;
     }
 
+    /**
+     * Create and add a ruleset
+     *
+     * @param string $selector Selector to use
+     * @param array<string, string> $properties CSS properties
+     *
+     * @return $this
+     */
     public function add(string $selector, array $properties): self
     {
         $this->children[] = static::create($selector, $properties);
@@ -65,6 +123,13 @@ class LessRuleset extends ArrayObject
         return $this;
     }
 
+    /**
+     * Add a ruleset
+     *
+     * @param LessRuleset $ruleset
+     *
+     * @return $this
+     */
     public function addRuleset(LessRuleset $ruleset): self
     {
         $this->children[] = $ruleset;
@@ -72,6 +137,11 @@ class LessRuleset extends ArrayObject
         return $this;
     }
 
+    /**
+     * Compile the ruleset to CSS
+     *
+     * @return string
+     */
     public function renderCss(): string
     {
         $parser = new Less_Parser(['compress' => true]);
@@ -80,6 +150,11 @@ class LessRuleset extends ArrayObject
         return $parser->getCss();
     }
 
+    /**
+     * Render the ruleset to LESS
+     *
+     * @return string
+     */
     protected function renderLess(): string
     {
         $less = [];
