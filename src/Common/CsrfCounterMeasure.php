d63c5a748750278dd33d7dcec549a832822f5e5d
Johannes Meyer <johannes.meyer@icinga.com>
2025-08-11 14:52:48 +0200

    CsrfCounterMeasure: Allow external control
    Allows to set the id using a setter, avoiding the
    need to access the session inside a form for example.
    A controller can now set the id. The same goes for
    deciding whether a token is required or not, which
    is necessary in unit tests, where it is not.
    
    `createCsrfCounterMeasure()` has now been deprecated
    in favor of `addCsrfCounterMeasure()` since no element
    can be returned if disabled and 99% of all usages
    immediately add the *created* element to the form anyway.

--- src/Common/CsrfCounterMeasure.php
+++ src/Common/CsrfCounterMeasure.php
@@ -8,12 +8,44 @@ use ipl\Html\FormElement\HiddenElement;
 
 trait CsrfCounterMeasure
 {
+    /** @var ?string The ID of the CSRF form element */
+    private ?string $csrfCounterMeasureId = null;
+
+    /** @var bool Whether to actually add the CSRF element to the form */
+    private bool $csrfCounterMeasureEnabled = true;
+
+    /**
+     * Set the ID for the CSRF form element
+     *
+     * @param string $id A unique ID that persists through different requests
+     *
+     * @return $this
+     */
+    public function setCsrfCounterMeasureId(string $id): static
+    {
+        $this->csrfCounterMeasureId = $id;
+
+        return $this;
+    }
+
+    /**
+     * Disable the CSRF form element
+     *
+     * @return void
+     */
+    public function disableCsrfCounterMeasure(): void
+    {
+        $this->csrfCounterMeasureEnabled = false;
+    }
+
     /**
      * Create a form element to countermeasure CSRF attacks
      *
      * @param string $uniqueId A unique ID that persists through different requests
      *
      * @return FormElement
+     *
+     * @deprecated Use {@see addCsrfCounterMeasure()} instead
      */
     protected function createCsrfCounterMeasure($uniqueId)
     {
@@ -52,5 +84,28 @@ trait CsrfCounterMeasure
         });
 
         return $element;
+    }
+
+    /**
+     * Add the CSRF form element to this form
+     *
+     * Does nothing if disabled via {@see disableCsrfCounterMeasure()}.
+     * Unless passed as argument, requires a unique ID to be set via {@see setCsrfCounterMeasureId()}.
+     *
+     * @param ?string $uniqueId A unique ID that persists through different requests
+     *
+     * @return void
+     */
+    protected function addCsrfCounterMeasure(?string $uniqueId = null): void
+    {
+        if (! $this->csrfCounterMeasureEnabled) {
+            return;
+        }
+
+        if ($uniqueId === null && $this->csrfCounterMeasureId === null) {
+            throw new Error('No CSRF counter measure ID set');
+        }
+
+        $this->addElement($this->createCsrfCounterMeasure($uniqueId ?? $this->csrfCounterMeasureId));
     }
 }
