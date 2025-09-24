280fb2db1cf25357aeba4c6ab3eb309b63f173a6
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2025-09-24 11:58:28 +0200

    CompatForm: Add method to apply new decorators

--- src/Compat/CompatForm.php
+++ src/Compat/CompatForm.php
@@ -3,7 +3,9 @@
 namespace ipl\Web\Compat;
 
 use http\Exception\InvalidArgumentException;
+use ipl\Html\Contract\FormElement;
 use ipl\Html\Contract\FormSubmitElement;
+use ipl\Html\Contract\HtmlElementInterface;
 use ipl\Html\Form;
 use ipl\Html\FormElement\SubmitButtonElement;
 use ipl\Html\FormElement\SubmitElement;
@@ -17,6 +19,64 @@ class CompatForm extends Form
     use Translation;
 
     protected $defaultAttributes = ['class' => 'icinga-form icinga-controls'];
+
+    /** @var bool Whether to disable the legacy form decorator */
+    private bool $disableLegacyDecorator = false;
+
+    /**
+     * Apply default element decorators
+     *
+     * This method must be called before adding any elements to the form.
+     *
+     * Calling this method disables the legacy decorator.
+     *
+     * @return $this
+     */
+    public function applyDefaultElementDecorators(): static
+    {
+        $this->disableLegacyDecorator = true;
+
+        $this->addElementDecoratorLoaderPaths([
+            ['ipl\\Web\\Compat\\FormDecorator', 'Decorator']
+        ]);
+
+        $this->setDefaultElementDecorators([
+            'Label',
+            [
+                'name' => 'HtmlTag',
+                'options' => [
+                    'tag' => 'div',
+                    'class' => 'control-label-group',
+                    'condition' => function (FormElement $element): bool {
+                        return ! $element instanceof FormSubmitElement
+                            && (! $element instanceof HtmlElementInterface || $element->getTag() !== 'fieldset');
+                    }
+                ]
+            ],
+            'Fieldset',
+            'Checkbox',
+            'RenderElement',
+            'Description',
+            [
+                'name' => 'HtmlTag',
+                'options' => [
+                    'tag' => 'div',
+                    'class' => 'control-group',
+                    'condition' => fn(FormElement $element): bool => ! $element instanceof FormSubmitElement
+                ]
+            ],
+            [
+                'name' => 'HtmlTag',
+                'options' => [
+                    'tag' => 'div',
+                    'class' => 'control-group form-controls',
+                    'condition' => fn(FormElement $element): bool => $element instanceof FormSubmitElement
+                ]
+            ],
+        ]);
+
+        return $this;
+    }
 
     /**
      * Render the content of the element to HTML
@@ -41,6 +101,10 @@ class CompatForm extends Form
 
     public function hasDefaultElementDecorator()
     {
+        if ($this->disableLegacyDecorator) {
+            return false;
+        }
+
         if (parent::hasDefaultElementDecorator()) {
             return true;
         }
