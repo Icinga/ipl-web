c330ed78bff8f5ae5a947f3667234ca460cec4ae
Johannes Meyer <johannes.meyer@icinga.com>
2025-10-01 09:27:58 +0200

    Deprecate the old `TermInput` class

---
+++ src/FormElement/TermInput.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ipl\Web\FormElement;
+
+use ipl\Html\FormElement\FormElements;
+
+/**
+ * A term input
+ *
+ * @deprecated Use {@see TermInputElement}, or actually, use {@see FormElements::createElement()} instead
+ */
+class TermInput extends TermInputElement
+{
+
+}
