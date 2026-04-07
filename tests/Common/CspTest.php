c8934de5937c656dd0d408f6c5ecbdfd7bb0ea38
Alexander Rieß <alexander.riess@icinga.com>
2026-04-07 09:44:27 +0200

    Add more tests

--- tests/Common/CspTest.php
+++ tests/Common/CspTest.php
@@ -84,6 +84,13 @@ class CspTest extends TestCase
         $csp->add('default-src', 'https://example.com');
     }
 
+    public function testFallbackToDefault()
+    {
+        $csp = new Csp();
+
+        $this->assertEquals(["'self'"], $csp->getDirective('script-src'));
+    }
+
     public function testAddDirectiveNameCapitals()
     {
         $this->expectException(InvalidArgumentException::class);
@@ -100,6 +107,94 @@ class CspTest extends TestCase
         $csp = new Csp();
 
         $csp->add('default-src:', 'https://example.com');
+    }
+
+    public function testAddWildcardEverything()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', '*');
+
+        $this->assertEquals(['*'], $csp->getDirective('script-src'));
+    }
+
+    public function testAddWildcard()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', 'https://*.example.com');
+        $csp->add('script-src', 'https://*.int.example.com');
+
+        $this->assertEquals(
+            ['https://*.example.com', 'https://*.int.example.com'],
+            $csp->getDirective('script-src'),
+        );
+    }
+
+    public function testAddMissingEndQuote()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $csp = new Csp();
+
+        $csp->add('script-src', "'self");
+    }
+
+    public function testAddMissingStartQuote()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $csp = new Csp();
+
+        $csp->add('script-src', "self'");
+    }
+
+    public function testAddScheme()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', 'https://*');
+        $csp->add('script-src', 'http:');
+
+        $this->assertEquals(['https://*', 'http:'], $csp->getDirective('script-src'));
+    }
+
+    public function testAddReportingName()
+    {
+        $csp = new Csp();
+
+        $csp->add('report-to', 'reporting-endpoint');
+
+        $this->assertEquals(['reporting-endpoint'], $csp->getDirective('report-to'));
+    }
+
+    /**
+     * @dataProvider providerInvalidWildcards
+     */
+    public function testAddInvalidWildcard(string $policy)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $csp = new Csp();
+
+        $csp->add('script-src', $policy);
+    }
+
+    public function providerInvalidWildcards(): array {
+        return [
+            ['https://example.com*'],
+            ['https://a*.example.com'],
+            ['https://*c.example.com'],
+            ['https://a*c.example.com'],
+            ['https://a*.int.example.com'],
+            ['https://*c.int.example.com'],
+            ['https://a*c.int.example.com'],
+            ['https://int.a*.example.com'],
+            ['https://int.*c.example.com'],
+            ['https://int.a*c.example.com'],
+            ['https://example.*'],
+            ['https://example.*om'],
+            ['https://example.c*m'],
+            ['https://example.co*'],
+            ['https://exa*ple.com'],
+        ];
     }
 
     public function testGetDirectives()
@@ -158,5 +253,100 @@ class CspTest extends TestCase
             ],
             $csp->getDirectives(),
         );
+    }
+
+    public function testEvaluateWildcardEverything()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', '*');
+
+        $this->assertTrue($csp->evaluateUrl('script-src', 'https://example.com'));
+    }
+
+    public function testEvaluateNone()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', "'none'");
+
+        $this->assertFalse($csp->evaluateUrl('script-src', 'https://example.com'));
+        $this->assertFalse($csp->evaluateUrl('script-src', 'http://example.com'));
+        $this->assertFalse($csp->evaluateUrl('script-src', 'test'));
+    }
+
+    public function testEvaluateNoneWithMultiplePolicies()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', "'none'");
+        $csp->add('script-src', 'https://example.com');
+
+        $this->assertTrue($csp->evaluateUrl('script-src', 'https://example.com'));
+        $this->assertFalse($csp->evaluateUrl('script-src', 'https://foo.com'));
+    }
+
+    public function testEvaluateSelf()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', "'self'");
+
+        // Note: This works because the request url for unit tests is always http://localhost
+        $this->assertTrue($csp->evaluateUrl('script-src', 'http://localhost'));
+    }
+
+    public function testEvaluateSchema()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', 'https:');
+
+        $this->assertTrue($csp->evaluateUrl('script-src', 'https://example.com'));
+        $this->assertTrue($csp->evaluateUrl('script-src', 'https://int.example.com'));
+        $this->assertTrue($csp->evaluateUrl('script-src', 'https://icinga.com'));
+        $this->assertFalse($csp->evaluateUrl('script-src', 'http://example.com'));
+    }
+
+    public function testEvaluateWildcardSchema()
+    {
+        $csp = new Csp();
+        $csp->add('script-src', 'https://*');
+
+        $this->assertTrue($csp->evaluateUrl('script-src', 'https://example.com'));
+        $this->assertTrue($csp->evaluateUrl('script-src', 'https://int.example.com'));
+        $this->assertTrue($csp->evaluateUrl('script-src', 'https://icinga.com'));
+        $this->assertFalse($csp->evaluateUrl('script-src', 'http://example.com'));
+    }
+
+    public function testEvaluatePathDirectory()
+    {
+        $csp = new Csp();
+        $csp->add('frame-src', 'https://example.com/blog/');
+
+        $this->assertTrue($csp->evaluateUrl('frame-src', 'https://example.com/blog/some-article.html'));
+        $this->assertTrue($csp->evaluateUrl('frame-src', 'https://example.com/blog/year/month/date/some-article.html'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'http://example.com/blog/'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'http://example.com/blog'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'http://example.com'));
+    }
+
+    public function testEvaluatePathFile()
+    {
+        $csp = new Csp();
+        $csp->add('frame-src', 'https://example.com/blog/some-article.html');
+
+        $this->assertTrue($csp->evaluateUrl('frame-src', 'https://example.com/blog/some-article.html'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'https://blog.example.com/blog/some-article.html'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'https://example.com/blog/another-article.html'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'https://example.com/feed/some-article.html'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'https://example.com/feed/some-article'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'https://icinga.com/blog/some-article.html'));
+    }
+
+    public function testEvaluateWildcardHost()
+    {
+        $csp = new Csp();
+        $csp->add('frame-src', 'https://*.example.com');
+
+        $this->assertTrue($csp->evaluateUrl('frame-src', 'https://example.com'));
+        $this->assertTrue($csp->evaluateUrl('frame-src', 'https://cdn.example.com'));
+        $this->assertTrue($csp->evaluateUrl('frame-src', 'https://monitoring.int.example.com'));
+        $this->assertFalse($csp->evaluateUrl('frame-src', 'https://icinga.com'));
     }
 }
