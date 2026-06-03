146f55f1f2bad2682a72909ac06105beb634aa98
Alexander A. Klimov <alexander.klimov@icinga.com>
2026-06-03 11:16:46 +0200

    CsrfCounterMeasure: accept Sec-Fetch-Site header, if available, instead of token
    This is especially useful if the session and token change suddenly, e.g. due to mod_auth_openidc.

--- tests/Common/CsrfCounterMeasureTest.php
+++ tests/Common/CsrfCounterMeasureTest.php
@@ -8,14 +8,28 @@ use ipl\Html\Form;
 use ipl\Html\FormElement\HiddenElement;
 use ipl\Tests\Web\TestCase;
 use ipl\Web\Common\CsrfCounterMeasure;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class CsrfCounterMeasureTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        unset($_SERVER['HTTP_SEC_FETCH_SITE']);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        unset($_SERVER['HTTP_SEC_FETCH_SITE']);
+    }
+
     public function testTokenCreation()
     {
         $token = $this->createElement();
 
         $this->assertInstanceOf(HiddenElement::class, $token);
+        $this->assertNotCount(0, $token->getValidators(), 'Token element must have at least one validator');
         $this->assertMatchesRegularExpression(
             '/ value="[^"]+\|[^"]+"/',
             (string) $token,
@@ -57,6 +71,47 @@ class CsrfCounterMeasureTest extends TestCase
         $token->isValid();
     }
 
+    public static function safeHeaderValueProvider(): array
+    {
+        return [
+            'same-origin' => ['same-origin'],
+            'none'        => ['none'],
+        ];
+    }
+
+    public function testAddThrowsForCrossSiteRequest(): void
+    {
+        $_SERVER['HTTP_SEC_FETCH_SITE'] = 'cross-site';
+
+        $this->expectException(Error::class);
+        $this->expectExceptionMessage('Rejecting cross-site request');
+
+        $this->makeForm()->ensureAssembled();
+    }
+
+    #[DataProvider('safeHeaderValueProvider')]
+    public function testCreateReturnsDummyElementForSafeRequest(string $headerValue): void
+    {
+        $_SERVER['HTTP_SEC_FETCH_SITE'] = $headerValue;
+
+        $element = $this->makeForm()->callCreate('uniqueId');
+
+        $this->assertInstanceOf(HiddenElement::class, $element);
+        $this->assertNotCount(0, $element->getValidators(), 'Dummy element must have at least one validator');
+        $element->setValue('garbage');
+        $this->assertTrue($element->isValid(), 'Dummy element should accept any value without validation');
+    }
+
+    public function testCreateThrowsForCrossSiteRequest(): void
+    {
+        $_SERVER['HTTP_SEC_FETCH_SITE'] = 'cross-site';
+
+        $this->expectException(Error::class);
+        $this->expectExceptionMessage('Rejecting cross-site request');
+
+        $this->makeForm()->callCreate('uniqueId');
+    }
+
     private function createElement(): FormElement
     {
         $form = new class extends Form {
@@ -71,5 +126,22 @@ class CsrfCounterMeasureTest extends TestCase
         return $form->setCsrfCounterMeasureId('uniqueId')
             ->ensureAssembled()
             ->getElement('CSRFToken');
+    }
+
+    private function makeForm()
+    {
+        return new class extends Form {
+            use CsrfCounterMeasure;
+
+            public function callCreate(string $id): FormElement
+            {
+                return $this->createCsrfCounterMeasure($id);
+            }
+
+            protected function assemble(): void
+            {
+                $this->addCsrfCounterMeasure('uniqueId');
+            }
+        };
     }
 }
