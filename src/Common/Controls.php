235f4ca8ecaf1f1ee49533f29939f3f264b597dc
Bastian Lederer <bastian.lederer@icinga.com>
2026-07-13 10:11:30 +0200

    `Controls`: Handle redirects and changes of default limit

--- src/Common/Controls.php
+++ src/Common/Controls.php
@@ -4,19 +4,31 @@ namespace ipl\Web\Common;
 
 use Icinga\Web\UrlParams;
 use ipl\Html\Form;
+use ipl\Web\Compat\CompatController;
+use ipl\Web\Control\LimitControl;
+use ipl\Web\Control\PaginationControl;
 use ipl\Web\Control\ViewModeSwitcher;
+use ipl\Web\Url;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Provides factory methods to prepare reusable web controls and allows to handle their requests
+ *
+ * @phpstan-require-extends CompatController
  */
 trait Controls
 {
     /** @var Form[] Controls registered for handling by {@see self::handleControls()} */
     private array $trackedControls = [];
 
+    /** @var ?Url Url to redirect to in {@see self::handleControls()} */
+    private ?Url $redirectUrl = null;
+
     /**
      * Create a {@see ViewModeSwitcher} control
+     *
+     * The control is registered via {@see self::trackControl()} and gets a default {@see Form::ON_SUBMIT} handler
+     * that writes the chosen view mode to the {@see self::getRedirectUrl()}.
      *
      * @param UrlParams $params The url params; the view mode param is shifted out of them
      * @param ?string $viewModeParam Custom view mode param, null uses the default of the given class
@@ -40,7 +52,34 @@ trait Controls
 
         $this->trackControl($viewModeSwitcher);
 
+        $viewModeSwitcher->on(ViewModeSwitcher::ON_SUBMIT, function (ViewModeSwitcher $switcher): void {
+            $this->getRedirectUrl()->setParam($switcher->getViewModeParam(), $switcher->getViewMode());
+        });
+
         return $viewModeSwitcher;
+    }
+
+    /**
+     * Double the default item limit and page size for `minimal` view mode
+     *
+     * @param ViewModeSwitcher $viewModeSwitcher
+     * @param LimitControl $limitControl
+     * @param ?PaginationControl $paginationControl
+     *
+     * @return void
+     */
+    protected function applyViewModeLimit(
+        ViewModeSwitcher $viewModeSwitcher,
+        LimitControl $limitControl,
+        ?PaginationControl $paginationControl = null
+    ): void {
+        if ($viewModeSwitcher->getViewMode() === 'minimal') {
+            $limitControl->setDefaultLimit($limitControl->getDefaultLimit() * 2);
+
+            $paginationControl
+                ?->setDefaultPageSize($paginationControl->getDefaultPageSize() * 2)
+                ->apply();
+        }
     }
 
     /**
@@ -58,7 +97,18 @@ trait Controls
     }
 
     /**
-     * Call {@see Form::handleRequest()} on every control in {@see self::$trackedControls}
+     * Get the Url {@see self::handleControls()} redirects to
+     *
+     * @return Url
+     */
+    protected function getRedirectUrl(): Url
+    {
+        return $this->redirectUrl ??= Url::fromRequest();
+    }
+
+    /**
+     * Call {@see Form::handleRequest()} on every control in {@see self::$trackedControls}. If {@see self::$redirectUrl}
+     * has been set, a redirect is performed afterwards.
      *
      * @param ServerRequestInterface $request
      *
@@ -68,6 +118,10 @@ trait Controls
     {
         foreach ($this->trackedControls as $control) {
             $control->handleRequest($request);
+        }
+
+        if ($this->redirectUrl !== null) {
+            $this->redirectNow($this->redirectUrl);
         }
 
         return $this;
